bbd662c21119d470ed4132d4318f0e49a37e9fb7
Facundo Abarca <inditex1@P20058.local>
2020-03-30 23:46:23 +0200

    se define ycrea colleción usuario. Autenticación de mongodb.

--- index.ts
+++ index.ts
@@ -1,16 +1,23 @@
 import Server from "./classes/server";
 import userRoutes from './routes/user-routes';
 import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+
 const server = new Server();
+
+
+// Body Parser (middleware)
+server.app.use(bodyParser.urlencoded({ extended: true }));
+server.app.use(bodyParser.json());
 
 
 // Rutas de mi app.
 server.app.use('/user', userRoutes);
 
 // Conectar db
-mongoose.connect('mongodb://localhost:27017/fotosgram',
+mongoose.connect('mongodb://fabarca:123456qA@localhost:27017/fotosgram',
 	{ useNewUrlParser: true, useCreateIndex: true }, (error) => {
-		if(error) {
+		if (error) {
 			throw error;
 		}
 
@@ -19,3 +26,4 @@ mongoose.connect('mongodb://localhost:27017/fotosgram',
 
 // Levantar express
 server.start(() => { });
+
--- routes/user-routes.ts
+++ routes/user-routes.ts
@@ -1,13 +1,36 @@
 import { Router, Request, Response } from "express";
+import { User } from '../models/user.model';
 
 const userRoutes = Router();
 
 userRoutes.get('/prueba', (req: Request, res: Response) => {
-
 	res.json({
 		ok: true,
 		message: 'Todo funciona bien'
 	});
+});
+
+userRoutes.post('/create', (req: Request, res: Response) => {
+
+	const user = {
+		name: req.body.name,
+		email: req.body.email,
+		password: req.body.password,
+		avatar: req.body.avatar
+	};
+
+	User.create(user).then(userDb => {
+		res.json({
+			ok: true,
+			user: userDb
+		});
+	}).catch( err => {
+		res.json({
+			ok: false,
+			error: err
+		})
+	});
+
 
 });
 
