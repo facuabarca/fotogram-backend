55ee9b91fe3dc5a610511f7599b0ba317b163e62
Facundo Abarca <inditex1@P20058.local>
2020-04-01 20:36:22 +0200

    se agrega userToken

--- models/user.model.ts
+++ models/user.model.ts
@@ -1,5 +1,6 @@
 
 import { Schema, model, Document } from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const usuarioSchema = new Schema({
 	name: {
@@ -21,11 +22,18 @@ const usuarioSchema = new Schema({
 	}
 });
 
+usuarioSchema.method('compararPassword', function (password: string = ''): boolean {
+	if (bcrypt.compareSync(password, this.password))
+		return true;
+	else return false;
+});
+
 interface IUser extends Document {
 	nombre: string;
 	email: string;
 	password: string;
-	avatar?: string
+	avatar?: string,
+	compararPassword(password: string): boolean
 }
 
 export const User = model<IUser>('User', usuarioSchema);
--- routes/user-routes.ts
+++ routes/user-routes.ts
@@ -1,5 +1,7 @@
 import { Router, Request, Response } from "express";
 import { User } from '../models/user.model';
+import bcrypt from 'bcrypt';
+import Token from '../classes/token';
 
 const userRoutes = Router();
 
@@ -10,21 +12,62 @@ userRoutes.get('/prueba', (req: Request, res: Response) => {
 	});
 });
 
+userRoutes.post('/login', (req: Request, res: Response) => {
+	const body = req.body;
+	User.findOne({ email: body.email }, (err, userDB) => {
+		if(err) throw err;
+
+		if(!userDB) {
+			return res.json({
+				ok: false,
+				mensaje: 'Usuario/Contraseña no son correctos'
+			});
+		}
+		if(userDB.compararPassword(body.password)) {
+			const userToken = Token.getJwtToken({
+				_id: userDB._id,
+				name: userDB.nombre,
+				email: userDB.email,
+				avatar: userDB.avatar
+			});
+			return res.json({
+				ok: true,
+				token: userToken
+			});
+		} else {
+			return res.json({
+				ok: false,
+				mensaje: 'Usuario/Contraseña no son correctos *******'
+			});
+		}
+		
+	});
+
+});
+
 userRoutes.post('/create', (req: Request, res: Response) => {
 
 	const user = {
 		name: req.body.name,
 		email: req.body.email,
-		password: req.body.password,
+		password: bcrypt.hashSync(req.body.password, 10),
 		avatar: req.body.avatar
 	};
 
+	
+
 	User.create(user).then(userDb => {
+		const userToken = Token.getJwtToken({
+			_id: userDb._id,
+			name: userDb.nombre,
+			email: userDb.email,
+			avatar: userDb.avatar
+		});
 		res.json({
 			ok: true,
-			user: userDb
+			user: userToken
 		});
-	}).catch( err => {
+	}).catch(err => {
 		res.json({
 			ok: false,
 			error: err
