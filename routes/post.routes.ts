1680ee1233301de5c78b4085a4cb2e848fe36e12
Facundo Abarca <inditex1@P20058.local>
2020-04-03 16:13:30 +0200

    se crea get para recibir posts paginados

--- routes/post.routes.ts
+++ routes/post.routes.ts
@@ -4,6 +4,25 @@ import { Post } from '../models/post.model';
 
 const postRoutes = Router();
 
+
+// Obtener post
+postRoutes.get('/', [verifyToken], async (req: any, res: Response) => { 
+
+	let pagina = parseInt(req.query.pagina) || 1;
+	let skip = pagina - 1;
+	skip = skip * 10;
+	const posts = await Post.find()
+							.sort({_id: -1})
+							.skip(skip)
+							.limit(10)
+							.populate('user', '-password')
+							.exec();
+	res.json({
+		ok: true,
+		posts
+	})
+});
+// crear post
 postRoutes.post('/', [verifyToken], (req: any, res: Response) => {
 
 	const body = req.body;
@@ -11,7 +30,7 @@ postRoutes.post('/', [verifyToken], (req: any, res: Response) => {
 	body.user = req.user._id;
 
 	Post.create( body ).then( async postDb => {
-		await postDb.populate('user').execPopulate();
+		await postDb.populate('user', '-password').execPopulate();
 		res.json({
 			ok: true,
 			post: postDb
